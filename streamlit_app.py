90b82f1133b939bae8b683741fa92ff2f0493dae
gauravgupta30 <79806890+gauravgupta30@users.noreply.github.com>
2023-04-07 16:30:51 +0530

    Update streamlit_app.py
    added try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#reads data from API in json
-#streamlit.text(fruityvice_response.json()) 
+try:
+ #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+ #streamlit.write('The user entered ', fruit_choice)
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+ else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  #display a data frame as an interactive object 
+  streamlit.dataframe(fruityvice_normalized)
 
-# convert each record in the list into a row of tabular form. 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#display a data frame as an interactive object 
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+ streamlit.error()
+
 streamlit.stop()
 
 #import snowflake.connector
