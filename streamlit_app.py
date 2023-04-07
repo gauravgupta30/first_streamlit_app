ed86b4455cb8b8a74a63de33bcbc60bec4451ba4
gauravgupta30 <79806890+gauravgupta30@users.noreply.github.com>
2023-04-07 14:46:40 +0530

    Update streamlit_app.py
    import snowflake connector library

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display a data frame as an interactive object 
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
