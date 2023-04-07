ce345851e1fa432aeba21cc935a90797a6eaf0eb
gauravgupta30 <79806890+gauravgupta30@users.noreply.github.com>
2023-04-07 12:41:58 +0530

    Update streamlit_app.py
    normalized json data from API

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#reads data from API in json
+streamlit.text(fruityvice_response.json()) 
+
+# convert each record in the list into a row of tabular form. 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#display a data frame as an interactive object 
+streamlit.dataframe(fruityvice_normalized)
